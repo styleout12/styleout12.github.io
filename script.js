f27448b2bca4ada022379aef2ef889823083efcf
abduofficial <126914093+abduofficial@users.noreply.github.com>
2023-03-31 23:28:05 +0200

    added files

---
+++ script.js
@@ -0,0 +1,9 @@
+const tabBtn = document.querySelectorAll(".tab");
+const tab =document.querySelectorAll(".tabShow");
+function tabs(panelIndex){
+    tab.forEach(function(node){
+        node.style.display = "none";
+    });
+    tab[panelIndex].style.display="block";
+}
+tabs(0);
